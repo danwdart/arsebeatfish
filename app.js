9e8c78b6a19cb10ee7f315a81a6d213122b3511b
Dan <git@dandart.co.uk>
2016-04-02 01:10:43 +0100

    updates, config demo and rest

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
 var express = require('express'),
-	serveStatic = require('serve-static'),
-	cookieParser = require('cookie-parser'),
-	session = require('express-session'),
-	bodyParser = require('body-parser'),
-	app = express(),
-	config = require('./config/config.json'),
-	routes = require('./routes')
-	port = process.env.PORT || process.env.VMC_APP_PORT || 3000;
+    serveStatic = require('serve-static'),
+    cookieParser = require('cookie-parser'),
+    session = require('express-session'),
+    bodyParser = require('body-parser'),
+    app = express(),
+    config = require('./config/config.json'),
+    routes = require('./lib/routes'),
+    db = require('./lib/db'),
+    port = process.env.PORT || 3000;
 
 app.use(serveStatic(__dirname + '/../public'));
 store = new session.MemoryStore;
 app.use(cookieParser());
 app.use(session({
- 	saveUninitialized: true,
- 	resave: true,
-  	secret: 'shhh',
-  	store: store
+     saveUninitialized: true,
+     resave: true,
+     secret: 'shhh',
+     store: store
 }));
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
 app.use(bodyParser.json());
 app.use(function(err, req, res, next) {
-  return res.send (500, {
-    error: err.message
-  });
+    return res.send(
+        500,
+        {
+            error: err.message
+        }
+    );
 });
-routes(express, app, config);
+var models = db(config);
+routes(express, app, config, models);
 app.listen(port, function() {
-  return console.log("Listening on " + port + "\nPress CTRL-C to stop server.");
+    return console.log("Listening on " + port + "\nPress CTRL-C to stop server.");
 });
